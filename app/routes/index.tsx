347bb6c288006e708ff2f6d41e6f4eb7535eb433
irisdew <ycleardew@gmail.com>
2022-01-09 15:09:22 +0900

    :lipstick: add basic css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,10 @@
+import type { LinksFunction } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
 export default function IndexRoute() {
     return <div>안녕 나는 Index Route야</div>
 }
